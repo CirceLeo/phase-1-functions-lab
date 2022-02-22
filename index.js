1fac61976011fb83fbeae8669624ff3f46e27916
Calypso Leonard <calypsorleonard@gmail.com>
2022-02-22 13:38:46 -0500

    commit tree 33d83ae96f84f1d76466123da4a0e6c5c7fb9446

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (location){
+    let HQ = 42;
+    if (location <= HQ) {
+        return HQ - location;
+    } else {
+        return location - HQ;
+    }
+}
+
+function distanceFromHqInFeet (location){
+    return distanceFromHqInBlocks(location)*264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    let distance = start - destination;
+    distance = Math.abs(distance);
+    return distance*264;
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400){
+        return 0;
+    } else if (distance > 2500){
+        return 'cannot travel that far';
+    } else if (distance > 2000){
+        return 25;
+    } else {
+        return (distance - 400) * .02;
+    }
+}
